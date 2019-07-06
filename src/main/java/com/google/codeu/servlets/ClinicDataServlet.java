c18baf878b0ea269c32e659d129103971eb5db28
bhaamati <bhaamati@princeton.edu>
2019-07-05 22:48:21 -0400

    Trying to use opencsv to parse data

--- src/main/java/com/google/codeu/servlets/ClinicDataServlet.java
+++ src/main/java/com/google/codeu/servlets/ClinicDataServlet.java
@@ -2,12 +2,22 @@ package com.google.codeu.servlets;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.CsvBindByName;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -16,64 +26,40 @@ import java.util.Scanner;
 @WebServlet("/clinics-data")
 public class ClinicDataServlet extends HttpServlet {
 
-  // Class constant for regex
-  private static final String SPLIT_REGEX_PATTERN = ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)"; 
+  private static final String CLINIC_DATA_CSV = "./WEB-INF/clinics-data-with-header.csv";
 
   private JsonArray clinicArray;
 
   @Override
   public void init() {
+    
     clinicArray = new JsonArray();
     Gson gson = new Gson();
-    Scanner scanner = new Scanner(getServletContext().getResourceAsStream("/WEB-INF/clinics-data.csv"));
-    while(scanner.hasNextLine()) {
-      String line = scanner.nextLine();
-      String[] cells = line.split(SPLIT_REGEX_PATTERN, -1);
-      
-      // Initializes to a default value in case the data is faulty
-      double lat = 0.0;
-      double lng = 0.0;
-      String title = "";
-      String address = "";
-      String phoneNum = "";
-      String [] services = new String[1];
-
-      // Implements checks on input data
-      if (cells.length >= 2) {
-        lat = Double.parseDouble(cells[0]);
-        lng = Double.parseDouble(cells[1]);
-      
-        // Add Title
-        if (cells.length >= 3) {
-          title = cells[2];
+    
+    try {
+            Reader reader = Files.newBufferedReader(Paths.get(CLINIC_DATA_CSV));
+            
+            CsvToBean<Clinic> csvToBean = new CsvToBeanBuilder<Clinic>(reader)
+                    .withType(Clinic.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+                    
+            //Iterator<Clinic> clinicIterator = csvToBean.iterator();
+            //System.out.println("hello");
+            //System.out.println("2");
+            while (csvToBean.iterator().hasNext()) {
+              System.out.println("hello");
+              Clinic clinic = csvToBean.iterator().next();
+              System.out.println("anything");
+              System.out.println(clinic.title);
+              
+              clinicArray.add(gson.toJsonTree(clinic));
+            }
         }
-
-        // Add Description
-        if (cells.length >= 4) {
-          address = cells[3];
-
-          // Get rid of quotes
-          if (address.charAt(0)=='\"') {
-            address = address.substring(1, address.length() - 1);
-          }
-        }
-       
-        if (cells.length >= 5) {
-          phoneNum = cells[4];
-        }
-
-        if (cells.length >= 6) {
-          // Get rid of quotes
-          line = cells[5];
-          line = line.substring(1, (line.length() - 1));  
-          services = line.split(";");
-        }
-        
-    }
- 
-    clinicArray.add(gson.toJsonTree(new Clinic(lat, lng, title, address, phoneNum, services)));
-    }
-    scanner.close();
+      catch (Exception IOException){
+        System.out.println(IOException);
+      }
+    
   }
 
   @Override
@@ -83,16 +69,22 @@ public class ClinicDataServlet extends HttpServlet {
   }
 
   // This class could be its own file if we needed it outside this servlet
-  private static class Clinic {
-    double lat;
-    double lng;
-    String title;
-    String address;
-    String phoneNum; 
-    String[] services;
+  public class Clinic {
+    @CsvBindByName
+    private double lat;
+    @CsvBindByName
+    private double lng;
+    @CsvBindByName
+    private String title;
+    @CsvBindByName
+    private String address;
+    @CsvBindByName
+    private String phoneNum; 
+    @CsvBindByName
+    private String services;
 
     private Clinic(double lat, double lng, String title, String address, String phoneNum, 
-                    String [] services) {
+                    String services) {
       this.lat = lat;
       this.lng = lng;
       this.title = title;
